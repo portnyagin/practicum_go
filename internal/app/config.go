10c535c59700035db92045e1ee215184c491a37b
portnyagin <shikakito@gmail.com>
2021-09-05 20:49:30 +0300

    # increment 6 env var FILE_STORAGE_PATH added

--- internal/app/config.go
+++ internal/app/config.go
@@ -3,6 +3,7 @@ package app
 type AppConfig struct {
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080/"`
+	FileStorage   string `env:"FILE_STORAGE_PATH" envDefault:"./data"`
 }
 
 func (c *AppConfig) validate() error {
