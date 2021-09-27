8646f00d35b948c49f5d4776fab88ff00cae6b17
portnyagin <shikakito@gmail.com>
2021-09-27 18:03:17 +0300

    # increment 12 fix error in query

--- internal/app/database/query.go
+++ internal/app/database/query.go
@@ -5,7 +5,7 @@ const InsertUserURL = "insert into user_urls (id, user_id, short_url, original_u
 	"values ( nextval('seq_user_urls'), $1, $2,$3)\n" +
 	"on conflict (user_id, original_url) do update set short_url = excluded.short_url"
 
-const InsertUserURL2 = "insert into user_urls2 (id, user_id,correlation_id, , original_url, short_url) \n\t" +
+const InsertUserURL2 = "insert into user_urls2 (id, user_id,correlation_id, original_url, short_url) \n\t" +
 	"values ( nextval('seq_user_urls'), $1, $2,$3, $4)\n" +
 	"on conflict (user_id, correlation_id, original_url) do update set short_url = excluded.short_url"
 
