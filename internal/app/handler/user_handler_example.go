16b51b5be01f810fc23da89265b1631e8cfd143a
portnyagin <shikakito@gmail.com>
2022-03-11 17:31:03 +0300

    increment 16 fix static tests

--- internal/app/handler/user_handler_example.go
+++ internal/app/handler/user_handler_example.go
@@ -14,6 +14,11 @@ func Example() {
 	client := http.Client{}
 	// Делаем первый запрос (не авторизщованный). Сохраняем сокращенный урл, получаем  cookie.
 	req, err := http.NewRequest("POST", "http://localhost:8080", strings.NewReader("original_url"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	req.Header.Add("Content-Type", "text/plain")
 	resp, err := client.Do(req)
 	if err != nil {
@@ -33,6 +38,10 @@ func Example() {
 
 	// Второй запрос с авторизацией. Получаем все  url пользователя.
 	req2, err := http.NewRequest("GET", "http://localhost:8080/api/user/urls", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	req2.AddCookie(cookieToken)
 	respPOST, err := client.Do(req2)
 	if err != nil {
@@ -41,5 +50,8 @@ func Example() {
 	defer respPOST.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(errors.Unwrap(err))
+	}
 	fmt.Println(string(body))
 }
